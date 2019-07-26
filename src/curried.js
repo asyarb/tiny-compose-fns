c1d4acc4fec63c2f5a52932abe1ac674e861ba03
Anthony Yarbrough <asyarb@hawaii.edu>
2019-07-26 08:51:11 -1000

    feat: curried fns

---
+++ src/curried.js
@@ -0,0 +1,82 @@
+import { curry } from 'curriable'
+
+import {
+  map as mapB,
+  filter as filterB,
+  forEach as forEachB,
+  join as joinB,
+  take as takeB,
+  concat as concatB,
+  find as findB,
+  some as someB,
+  flatDepth as flatDepthB,
+  flatMap as flatMapB,
+  slice as sliceB,
+  includes as includesB,
+  sortBy as sortByB,
+  indexOf as indexOfB,
+  lastIndexOf as lastIndexOfB,
+  get as getB,
+  has as hasB,
+  merge as mergeB,
+  mergeDeep as mergeDeepB,
+  startsWith as startsWithB,
+  replace as replaceB,
+  split as splitB,
+} from '../src'
+
+export const map = curry(mapB)
+export const filter = curry(filterB)
+export const forEach = curry(forEachB)
+export const join = curry(joinB)
+export const take = curry(takeB)
+export const concat = curry(concatB)
+export const find = curry(findB)
+export const some = curry(someB)
+export const flatDepth = curry(flatDepthB)
+export const flatMap = curry(flatMapB)
+export const slice = curry(sliceB)
+export const includes = curry(includesB)
+export const sortBy = curry(sortByB)
+export const indexOf = curry(indexOfB)
+export const lastIndexOf = curry(lastIndexOfB)
+export const get = curry(getB)
+export const has = curry(hasB)
+export const merge = curry(mergeB)
+export const mergeDeep = curry(mergeDeepB)
+export const startsWith = curry(startsWithB)
+export const replace = curry(replaceB)
+export const split = curry(splitB)
+
+export {
+  noop,
+  isEmpty,
+  isNull,
+  isUndefined,
+  isString,
+  isNumber,
+  isPlainObject,
+  isFunction,
+  first,
+  last,
+  tail,
+  fromEntries,
+  entries,
+  keys,
+  values,
+  allProm,
+  resolve,
+  lowercase,
+  camelCase,
+  UPPERCASE,
+  Upperfirst,
+  trim,
+  uniq,
+  shuffle,
+  flatDeep,
+  flat,
+  compact,
+  reverse,
+  sort,
+  size,
+} from '../src'
