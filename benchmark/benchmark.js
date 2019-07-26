b2a95131a9c33538ca7d88361ab96bd3b4971544
Anthony Yarbrough <asyarb@hawaii.edu>
2019-07-26 09:51:51 -1000

    docs: compose benchmark

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -1,21 +1,37 @@
 const Benchmark = require('benchmark')
+const compose = require('compose-tiny')
+
 const _ = require('lodash/fp')
-const __ = require('lodash')
-const { map } = require('../dist/index.js')
-const { map: curryMap } = require('../dist/fp.js')
+const {
+  map,
+  compact,
+  camelCase,
+  filter,
+  uniq,
+  isString,
+} = require('../dist/fp.js')
 
 // Testing Setup
-const arr = []
-for (let i = 0; i < 100; i++) arr[i] = i
-const strAndRoot = x => 'Some string here' + Math.sqrt(x * x)
+const arr = [1, 2, 3, 1, 2, 3, undefined, null, 'foo bar baz', 'hello world']
 
 const suite = new Benchmark.Suite()
 
-suite.add('lodash/fp', () => _.map(strAndRoot, arr))
-suite.add('lodash', () => __.map(arr, strAndRoot))
-suite.add('tinyFns', () => map(strAndRoot, arr))
-suite.add('curryTinyFns', () => curryMap(strAndRoot, arr))
-suite.add('vanilla', () => arr.map(strAndRoot))
+suite.add('lodash', () => {
+  _.compose(
+    _.map(camelCase),
+    _.filter(isString),
+    _.uniq,
+    _.compact
+  )(arr)
+})
+suite.add('tinyFns', () => {
+  compose(
+    map(camelCase),
+    filter(isString),
+    uniq,
+    compact
+  )(arr)
+})
 
 suite
   .on('cycle', event => console.log(String(event.target)))
